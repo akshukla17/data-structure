5ea6ad0020342f941557500d7d7735382d93554b
Ajay Shukla <ajayshukla.bhu@gmail.com>
2019-04-13 08:41:02 +0530

    adding binary tree problems

---
+++ src/com/tree/FindDiameter.java
@@ -0,0 +1,44 @@
+package com.tree;
+
+
+public class FindDiameter {
+	static Node root;
+	public FindDiameter() {
+		root =null;
+	}
+	public FindDiameter(int d) {
+		root = new Node(d);
+	}
+	public static void main(String []args) {
+		FindDiameter tree = new FindDiameter(1);
+		root.left = new Node(2);
+		
+		root.left.left = new Node(4);
+		root.left.left.left = new Node(6);
+		root.left.left.right = new Node(7);
+		root.left.left.right.left = new Node(9);
+		root.left.left.right.left.right = new Node(12);
+		
+		root.left.right = new Node(5);
+		root.left.right.right = new Node(8);
+		root.left.right.right.left = new Node(10);
+		root.left.right.right.right = new Node(11);
+		root.left.right.right.right.left = new Node(12);
+		
+		root.right = new Node(3);
+		
+		int diameter = getDia(root);
+		System.out.println("Diameter of the graph : "+ diameter);
+	}
+	public static int getDia(Node root) {
+		if(root == null) return 0;
+		int heightLeft = findHeight(root.left);
+		int heightRight = findHeight(root.right);
+		return Math.max(heightLeft + heightRight +1, Math.max(getDia(root.left), getDia(root.right)));
+	}
+	public static int  findHeight(Node node) {
+		if(node == null) return 0;
+		return Math.max( findHeight(node.left), findHeight(node.right)) +1;
+	}
+
+}
